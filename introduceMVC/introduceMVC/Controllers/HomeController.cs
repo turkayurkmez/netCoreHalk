6cd5117d7b6063a8b9358bcd495d14ec5c1b01d1
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-28 15:30:50 +0300

    Basic structure with .net core mvc application

---
+++ introduceMVC/introduceMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using introduceMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace introduceMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Sehirler = new string[] { "Eskişehir", "İstanbul", "İzmir" };
+            ViewBag.Ad = "Türkay Ürkmez";
+            ViewBag.Saat = DateTime.Now.Hour;
+            ViewBag.Sehir = "Eskişehir";
+
+            var products = new List<Product>()
+            {
+                new() { Id=1, Name="Samsung", Description="Android", Price=1500},
+                new() { Id=2, Name="IPhone", Description="IOS", Price=1500},
+                new() { Id=3, Name="Huawei", Description="Android", Price=1500},
+
+            };
+
+            return View(products);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Connect()
+        {
+            return View();
+        }
+    }
+}
--- introduceMVC/introduceMVC/Program.cs
+++ introduceMVC/introduceMVC/Program.cs
@@ -1,6 +1,29 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+﻿var builder = WebApplication.CreateBuilder(args);
 
-app.MapGet("/", () => "Hello World!");
+//uygulamanızda kullanacağınız tüm alt yapıyı buradan ekleyeceksiniz...
+builder.Services.AddControllersWithViews();
+
+
+//build edildikten sonra uygulamanız oluşacak
+var app = builder.Build();
+/*
+ * Middleware: Bir httpRequest üzerinde gerçekleştirilen her bir adım.
+ * Pipeline: Middleware'lerin hangi sırayla çalışacağını belirleyen akış:
+ * 
+ * app.UseEtiket() --> şişelere etiket bass
+ * app.UseSodaDoldur() --> şişeleri doldur
+ * app.UseKapat() --> kapaklarını kapat
+ * app.UsePaketle() --> Şişeleri paketle
+ */
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+//app.UseAuthorization();
+
+
+
+app.MapControllerRoute(name: "default",
+                       pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
