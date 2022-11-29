c0f98281879b01bdc53733afb53f4629b081e0b5
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-29 16:21:28 +0300

    Lifecycle of Dependency Injection instance.

---
+++ DependencyIjectionLifeCycle/DependencyIjectionLifeCycle/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using DependencyIjectionLifeCycle.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DependencyIjectionLifeCycle.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IScopedGuidGenerator _scoped;
+        private readonly ISingletonGuidGenerator _singleton;
+        private readonly ITransientGuidGenerator _transient;
+        GuidService _guidService;
+
+        public HomeController(ILogger<HomeController> logger, IScopedGuidGenerator scoped, ISingletonGuidGenerator singleton, ITransientGuidGenerator transient, GuidService guidService)
+        {
+            _logger = logger;
+            _scoped = scoped;
+            _singleton = singleton;
+            _transient = transient;
+            _guidService = guidService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Transient = _transient.Guid.ToString();
+            ViewBag.Scoped = _scoped.Guid.ToString();
+            ViewBag.Singleton = _singleton.Guid.ToString();
+
+            ViewBag.ServiceTransient = _guidService._transient.Guid.ToString();
+            ViewBag.ServiceScoped = _guidService._scoped.Guid.ToString();
+            ViewBag.ServiceSingleton = _guidService._singleton.Guid.ToString();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DependencyIjectionLifeCycle/DependencyIjectionLifeCycle/Models/IGuidGenerator.cs
@@ -0,0 +1,64 @@
+﻿namespace DependencyIjectionLifeCycle.Models
+{
+    public interface IGuidGenerator
+    {
+        Guid Guid { get; }
+    }
+
+    public interface IScopedGuidGenerator : IGuidGenerator
+    {
+
+    }
+
+    public interface ITransientGuidGenerator : IGuidGenerator
+    {
+
+    }
+    public interface ISingletonGuidGenerator : IGuidGenerator
+    {
+
+    }
+
+
+    public class ScopedGuidGenerator : IScopedGuidGenerator
+    {
+        public ScopedGuidGenerator()
+        {
+            this.Guid = Guid.NewGuid();
+        }
+        public Guid Guid { get; }
+    }
+
+    public class SingletonGuidGenerator : ISingletonGuidGenerator
+    {
+        public SingletonGuidGenerator()
+        {
+            this.Guid = Guid.NewGuid();
+        }
+        public Guid Guid { get; }
+    }
+
+    public class TransientGuidGenerator : ITransientGuidGenerator
+    {
+        public TransientGuidGenerator()
+        {
+            this.Guid = Guid.NewGuid();
+        }
+        public Guid Guid { get; }
+    }
+
+    public class GuidService
+    {
+        public readonly ITransientGuidGenerator _transient;
+        public readonly IScopedGuidGenerator _scoped;
+        public readonly ISingletonGuidGenerator _singleton;
+
+        public GuidService(ITransientGuidGenerator transient, IScopedGuidGenerator scoped, ISingletonGuidGenerator singleton)
+        {
+            _transient = transient;
+            _scoped = scoped;
+            _singleton = singleton;
+        }
+    }
+
+}
